7d3bd228faa59f97544dde4e7723d057017811ce
shanning <shanningdobgima@gmail.com>
2019-07-19 11:37:04 +0200

    test for create

--- 17-07-2019/src/main/java/controller/Lanceur.java
+++ 17-07-2019/src/main/java/controller/Lanceur.java
@@ -3,7 +3,8 @@ package controller;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		
+		 FindByName f= new FindByName();
+		 f.create("test5","test5", "vill5", "rue5", "ville5", "rue5");
 	}
 
 }
